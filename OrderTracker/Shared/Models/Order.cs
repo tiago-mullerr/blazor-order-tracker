4e030a87bd297accda1b9bcb98c021751d9bcc8f
Tiago Muller <tiagomullerdeoliveira@hotmail.com>
2023-12-14 10:54:50 -0300

    Replacing by new project

---
+++ OrderTracker/Shared/Models/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTracker.Shared.Models
+{
+    public class Order
+    {
+        public Order()
+        {
+            Customer = new Customer();
+            Products = new List<Product>();
+        }
+
+        public int Id { get; set; }
+
+        public decimal Amount
+        {
+            get
+            {
+                return Products.Sum(s => s.Price * s.Quantity);
+            }
+        }
+
+        public string FormattedAmount
+        {
+            get
+            {
+                return $"${Amount}";
+            }
+        }
+
+        public Customer Customer { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
