6c4804be2421e662ee6db4d2ab7b3685102ac1d1
Tiago Muller <tiagomullerdeoliveira@hotmail.com>
2023-12-20 13:38:12 -0300

    State management using state container

--- OrderTracker/Client/Program.cs
+++ OrderTracker/Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OrderTracker.Client;
+using OrderTracker.Shared.Infrastructure;
 using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<StateContainer>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ OrderTracker/Shared/Infrastructure/StateContainer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTracker.Shared.Infrastructure
+{
+    public class StateContainer
+    {
+        private Dictionary<string, object> States { get; set; } = new Dictionary<string, object>();
+
+        public T? GetState<T>(string keyName) where T : class
+        {
+            States.TryGetValue(keyName, out object? state);
+
+            if (state != null)
+            {
+                var result = state as T;
+                return result;
+            }
+
+            return null;
+        }
+
+        public void SetState(string keyName, object state)
+        {
+            States.TryAdd(keyName, state);
+        }
+
+        public void ClearState(string keyName)
+        {
+            States.Remove(keyName);
+        }
+    }
+}
